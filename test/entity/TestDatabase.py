bf02b8de691a15e8618d6ae5be5253b6b8c32065
Daniel Chia <dchia03@gmail.com>
2019-12-21 22:23:28 +0800

    feat(redevelop): #1 added test case for Database

--- test/entity/TestDatabase.py
+++ test/entity/TestDatabase.py
@@ -1,7 +1,14 @@
 import unittest
-from main.entity import Database
+from main.entity.Database import Database
+import properties as props
 
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
-        self.database = None
+        self.database = Database(props.mongo_client_str, "Test")
+
+    def test_db(self):
+        assert(self.database.get_database() is not None)
+
+    def test_db_name(self):
+        assert(self.database.get_database_name() == "Test")
